0231991580aa0e0545126fb39b899149fd2b5964
goldshoot0720 <goldshoot0720@gmail.com>
2025-06-29 17:43:19 +0800

    wranglertoml indexjs

--- remix202507050418plus-git.bat
+++ remix202507050418plus-git.bat
@@ -1,7 +1,7 @@
 cd "C:\Users\chbon\Documents\remix202507050418plus"
 git init
 git add .
-git commit -m "netlify"
+git commit -m "wranglertoml indexjs"
 git remote add origin https://github.com/abuhg17/remix202507050418plus.git
 git checkout -b main
 git push -u origin main --force
